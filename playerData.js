f7ee1ee99595b93a9a04a4c46622bef28917a564
MatthewKaplan <kaplan.matthew.p@gmail.com>
2020-02-26 18:00:19 -0700

    Create mock data to seed through

---
+++ playerData.js
@@ -0,0 +1,19 @@
+let playerData = [
+    {
+        first_name: "Tiger",
+        last_name: "Woods",
+        score: "70"
+    },
+    {
+        first_name: "Corey",
+        last_name: "Conners",
+        score: "66"
+    },
+    {
+        first_name: "Patrick",
+        last_name: "Reed",
+        score: "67"
+    }
+]
+
+module.exports = playerData;
